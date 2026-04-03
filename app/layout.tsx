af1ac751cb1d520e50f861f99f0fcf9f66aef1b0
birdsinspace-gif <birdsinspace@gmail.com>
2026-04-02 21:53:01 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,10 +13,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link
-          rel="preconnect"
-          href="https://fonts.googleapis.com"
-        />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
           href="https://fonts.gstatic.com"
