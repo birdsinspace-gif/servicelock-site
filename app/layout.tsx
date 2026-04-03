781eb8e7a924c207cc69031ecedb732061dd800d
birdsinspace-gif <birdsinspace@gmail.com>
2026-04-02 22:08:18 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
           crossOrigin=""
         />
         <link
-          href="https://fonts.googleapis.com/css2?family=Archivo+Black&family=Russo+One&family=Teko:wght@500;600;700&family=Bebas+Neue&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap"
           rel="stylesheet"
         />
       </head>
