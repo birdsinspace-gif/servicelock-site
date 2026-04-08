2066ab61dafc389ff18fb23bec28a8fd5f445499
birdsinspace-gif <birdsinspace@gmail.com>
2026-04-07 20:34:40 -0400

    Update twilio-voice.ts

--- servicelock-project/api/twilio-voice.ts
+++ servicelock-project/api/twilio-voice.ts
@@ -1,19 +1,15 @@
-import twilio from 'twilio';
-
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).send('Method Not Allowed');
   }
 
-  const twiml = new twilio.twiml.VoiceResponse();
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>
+<Response>
+  <Say voice="alice">
+    Hi, thanks for calling ServiceLock. We just missed your call, but we will text you right now so you can tell us what you need.
+  </Say>
+</Response>`;
 
-  twiml.say(
-    { voice: 'alice' },
-    'Hi, thanks for calling ServiceLock. We just missed your call, but we will text you right now so you can tell us what you need.'
-  );
-
-  return res
-    .status(200)
-    .setHeader('Content-Type', 'text/xml')
-    .send(twiml.toString());
+  res.setHeader('Content-Type', 'text/xml');
+  return res.status(200).send(xml);
 }
